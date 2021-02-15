bfdd239bc7d6b99f35e9e01a37175b69bfd54dd7
YG Lee <yglee@YGs-MacBook-Pro.local>
2021-02-15 15:12:52 -0800

    Validation Messages

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser'); // handling all the parsing 
+const bodyParser = require('body-parser'); 
 
 app.use(bodyParser.json());
 
 app.post('/', (req,res) => {
     var email = req.body.email;
     var amount = req.body.amount;
+    
+    if(amount <= 1){
+        return_info = {};
+        return_info.error = true;
+        return_info.message = "The amount should be greater than 1";
+        return res.send(return_info);
+    }
+    
     res.send({"amount" : amount, "email": email});
 });
 
