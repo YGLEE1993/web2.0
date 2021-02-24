652f533b3527df56504000558d013d47455e5d87
YG Lee <yglee@YGs-MacBook-Pro.local>
2021-02-24 15:28:52 -0800

    design updated

--- public/index.js
+++ public/index.js
@@ -1,69 +1,135 @@
 function App() {
-    const [totalAmount, setTotalAmount] = React.useState("1000");
-    const [amount, setAmount] = React.useState('100');
-    const [email, setEmail] = React.useState('');
+  const [totalAmount, setTotalAmount] = React.useState("0");
+  const [amount, setAmount] = React.useState("");
+  const [email, setEmail] = React.useState("");
 
-    React.useEffect(async () => {
-        const result = await axios.get('get_total_amount');
-        setTotalAmount(result.data["0"].total_amount)
-    }, [])
+  React.useEffect(async () => {
+    const result = await axios.get("get_total_amount");
+    setTotalAmount(result.data["0"].total_amount);
+  }, []);
 
-    const onSubmit = async (e) => {
-        e.preventDefault();
-        const response = await axios.post('/post_info', {
-            amount: amount,
-            email: email,
-        })
-        window.location.href = response.data;
-    }
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const response = await axios.post("/post_info", {
+      amount: amount,
+      email: email,
+    });
+    window.location.href = response.data;
+  };
 
   return (
-    // <div className="App">
-    //     <h1>Web3.0 Lottery App</h1>
-    //     <p>Total lottery amount is {totalAmount} </p>
-    //     <form onSubmit={onSubmit}>
-    //         <input placeholder="amount" value={amount} onChange = {(e) => setAmount(e.target.value)}/>
-    //         <input placeholder="email" value={email} onChange = {(e) => setEmail(e.target.value)} />
-    //         <button type="submit">Participate</button>
-    //     </form>
-    // </div>
-    <div className="container">
-            <div className="col-md-12">
-              <div className="card text-center">
-                  <div className="card-header">
-                    <h1>TOTAL WINNING POOL IS </h1>
-                    <div className ="block">
-                        <div className="circle">
-                          <p>{totalAmount}</p>
-                        </div>
-                    </div>
-                  </div>
-                  <div className="card-body">
-                  <form onSubmit={onSubmit}>
-                      <div className="form-group">
-                        <label >Email address</label>
-                        <input type="email" className="form-control" placeholder="Enter email"
-                          onChange = {(e) => setEmail(e.target.value)}
-                        />
-                        <small className="form-text text-muted">Well never share your email with anyone else.</small>
-                      </div>
-                      <div className="form-group">
-                        <label>Amount</label>
-                        <input type="number" className="form-control"  placeholder="Enter Amount"
-                          value={amount}
-                          onChange = {(e) => setAmount(e.target.value)}
-                        />
-                        <small className="form-text text-muted">Enter the amount you would like to participate with</small>
-                      </div>
-                      <button type="submit" className="btn btn-primary">Submit</button>
-                    </form>
-                  </div>
+    <div className="container" style={{ width: "500px" }}>
+      <div className="col-md-12">
+        <nav
+          class="navbar navbar-light rounded"
+          style={{
+            marginTop: "50px",
+            border: "2px solid black",
+            backgroundColor: "f5f7f7",
+          }}
+        >
+          <a style={{ letterSpacing: "2px" }} class="navbar-brand" href="#">
+            <img
+              src="./project-bg.png"
+              width="40"
+              height="40"
+              class="d-inline-block align-top"
+              alt="lottery-logo"
+            />{" "}
+            Lottery 2.0
+          </a>
+        </nav>
+        <div
+          className="card text-center"
+          style={{ backgroundColor: "#0e1117" }}
+        >
+          <div className="card-header">
+            {/* <h1 className="lottery-pool" style={{ color: "white" }}>
+              Total Pool{" "}
+            </h1> */}
+            <div className="block">
+              <div className="circle">
+                <p>${totalAmount}</p>
               </div>
+              <p
+                style={{
+                  color: "white",
+                  fontSize: "12px",
+                  letterSpacing: "2px",
+                }}
+              >
+                TOTAL POOL
+              </p>
             </div>
+          </div>
+          <div className="card-body" style={{ backgroundColor: "white" }}>
+            <form onSubmit={onSubmit}>
+              <div className="form-group">
+                <p
+                  style={{
+                    fontSize: "17px",
+                    textAlign: "left",
+                    // marginBottom: "3px",
+                    margin: "3px",
+                    fontWeight: "500",
+                  }}
+                >
+                  Email address
+                </p>
+                <input
+                  type="email"
+                  className="form-control"
+                  placeholder="Enter email"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <small
+                  style={{ textAlign: "left", marginLeft: "5px" }}
+                  className="form-text text-muted"
+                >
+                  We'll never share your email with anyone else.
+                </small>
+              </div>
+              <div className="form-group">
+                <p
+                  style={{
+                    fontSize: "17px",
+                    textAlign: "left",
+                    // marginBottom: "3px",
+                    margin: "3px",
+                    fontWeight: "500",
+                  }}
+                >
+                  Amount
+                </p>
+                <input
+                  type="number"
+                  className="form-control"
+                  placeholder="$100"
+                  value={amount}
+                  onChange={(e) => setAmount(e.target.value)}
+                />
+                <small className="form-text text-muted">
+                  Enter the amount you would like to participate with.
+                </small>
+              </div>
+              <button
+                type="submit"
+                style={{
+                  backgroundColor: "#426e99",
+                  color: "white",
+                  letterSpacing: "1px",
+                  fontSize: "17px",
+                }}
+                className="btn btn-outline-dark"
+              >
+                PARTICIPATE
+              </button>
+            </form>
+          </div>
         </div>
+      </div>
+    </div>
   );
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-
+ReactDOM.render(<App />, document.getElementById("root"));
--- server.js
+++ server.js
@@ -1,203 +1,220 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const bodyParser = require('body-parser'); 
-const { save_user_information, get_total_amount, get_list_of_participants,  delete_user} = require("./models/server-db");
-const path = require('path');
-const publicPath = path.join(__dirname, './public');
-const paypal = require('paypal-rest-sdk');
-const session = require('express-session');
+const bodyParser = require("body-parser");
+const {
+  save_user_information,
+  get_total_amount,
+  get_list_of_participants,
+  delete_user,
+} = require("./models/server-db");
+const path = require("path");
+const publicPath = path.join(__dirname, "./public");
+const paypal = require("paypal-rest-sdk");
+const session = require("express-session");
 
-app.use(session (
-    {
-    secret: 'my decentralize app',
-    cookie : {maxAge: 60000}
-    }
-))
+app.use(
+  session({
+    secret: "my decentralize app",
+    cookie: { maxAge: 60000 },
+  })
+);
 /* handling all the parsing */
 app.use(bodyParser.json());
-app.use(express.static(publicPath))
+app.use(express.static(publicPath));
 
 /* paypal configuration */
 paypal.configure({
-    'mode': 'sandbox', //sandbox or live
-    'client_id': 'AXhFIq22tWqZ1wQDdQWBjnRTUUvaZFs7xHM4ukTp3gUOr2uyaNi-r48NbWiHiM57SA0E1J7jzZcCSS8f',
-    'client_secret': 'EJFijl_dJlFtlPqFbwnOPisIj1W4kPheWRipFoA2-FXZJSL4xfUOkVSrQsVlwz-pl029SxctjIHJDOxO'
+  mode: "sandbox", //sandbox or live
+  client_id:
+    "AXhFIq22tWqZ1wQDdQWBjnRTUUvaZFs7xHM4ukTp3gUOr2uyaNi-r48NbWiHiM57SA0E1J7jzZcCSS8f",
+  client_secret:
+    "EJFijl_dJlFtlPqFbwnOPisIj1W4kPheWRipFoA2-FXZJSL4xfUOkVSrQsVlwz-pl029SxctjIHJDOxO",
 });
 
-app.post('/post_info', async (req, res) => {
-    var email = req.body.email;
-    var amount = req.body.amount;
-    
-    if(amount <= 1){
-        return_info = {};
-        return_info.error = true;
-        return_info.message = "The amount should be greater than 1";
-        return res.send(return_info);
-    }
-    var fee_amount = amount * 0.9;
-    var result = await save_user_information({"amount" : fee_amount, "email": email}); //save to DB
-    req.session.paypal_amount = amount;
+app.post("/post_info", async (req, res) => {
+  var email = req.body.email;
+  var amount = req.body.amount;
 
-    /* Invoke paypal rest api */
-    var create_payment_json = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
+  if (amount <= 1) {
+    return_info = {};
+    return_info.error = true;
+    return_info.message = "The amount should be greater than 1";
+    return res.send(return_info);
+  }
+  var fee_amount = amount * 0.9;
+  var result = await save_user_information({
+    amount: fee_amount,
+    email: email,
+  }); //save to DB
+  req.session.paypal_amount = amount;
+
+  /* Invoke paypal rest api */
+  var create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "Lottery",
+              sku: "Funding",
+              price: amount,
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
         },
-        "redirect_urls": {
-            "return_url": "http://localhost:3000/success",
-            "cancel_url": "http://localhost:3000/cancel"
+        amount: {
+          currency: "USD",
+          total: amount,
         },
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "Lottery",
-                    "sku": "Funding",
-                    "price": amount,
-                    "currency": "USD",
-                    "quantity": 1
-                }]
-            },
-            "amount": {
-                "currency": "USD",
-                "total": amount
-            },
-            'payee': {
-                'email': "manager@decentralize-lottery.io"
-            },
-            "description": "Lottery purchase"
-        }]
-    };
+        payee: {
+          email: "manager@decentralize-lottery.io",
+        },
+        description: "Lottery purchase",
+      },
+    ],
+  };
 
-
-    paypal.payment.create(create_payment_json, function (error, payment) {
-        if (error) {
-            throw error;
-        } else {
-            console.log("Create Payment Response");
-            console.log(payment);
-            for(var i=0; i < payment.links.length; i++){
-                console.log(payment.links[i].rel)
-                if(payment.links[i].rel == 'approval_url'){
-                    return res.send(payment.links[i].href); //redirect user 
-                }
-            }
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Create Payment Response");
+      console.log(payment);
+      for (var i = 0; i < payment.links.length; i++) {
+        console.log(payment.links[i].rel);
+        if (payment.links[i].rel == "approval_url") {
+          return res.send(payment.links[i].href); //redirect user
         }
-    });
-});
-
-app.get('/success', (req, res) => {
-    const payerId = req.query.payerId;
-    const paymentId = req.query.paymentId;
-
-    var execute_payment_json = {
-        "payer_id" : payerId,
-        "transactions": [{
-            "amount" : {
-                "currency" : "USD",
-                "total": req.session.paypal_amount 
-            }
-        }]
-    };
-
-    paypal.payment.execute(paymentId, execute_payment_json, function(error, payment){
-        if(error){
-            console.log(error.response);
-            throw error;
-        }else{
-            console.log(payment)
-        }
-    });
-
-    /* Delete all mysql users */ 
-    if(req.session.winner_picked){
-        var deleted = delete_user();
+      }
     }
-    req.session.winner_picked = false;
-    res.redirect('http://localhost:3000')
+  });
 });
 
-app.get('/get_total_amount', async (req, res) => {
-    const result = await get_total_amount();
-    res.send(result)
-})
+app.get("/success", (req, res) => {
+  const payerId = req.query.payerId;
+  const paymentId = req.query.paymentId;
 
-app.get('/pick_winner', async (req, res) => {
-    const result = await get_total_amount();
-    const total_amount = result[0].total_amount;
-    req.session.paypal_amount = total_amount;
-    
-    /* Placeholder for picking the winner
+  var execute_payment_json = {
+    payer_id: payerId,
+    transactions: [
+      {
+        amount: {
+          currency: "USD",
+          total: req.session.paypal_amount,
+        },
+      },
+    ],
+  };
+
+  paypal.payment.execute(
+    paymentId,
+    execute_payment_json,
+    function (error, payment) {
+      if (error) {
+        console.log(error.response);
+        throw error;
+      } else {
+        console.log(payment);
+      }
+    }
+  );
+
+  /* Delete all mysql users */
+  if (req.session.winner_picked) {
+    var deleted = delete_user();
+  }
+  req.session.winner_picked = false;
+  res.redirect("http://localhost:3000");
+});
+
+app.get("/get_total_amount", async (req, res) => {
+  const result = await get_total_amount();
+  res.send(result);
+});
+
+app.get("/pick_winner", async (req, res) => {
+  const result = await get_total_amount();
+  const total_amount = result[0].total_amount;
+  req.session.paypal_amount = total_amount;
+
+  /* Placeholder for picking the winner
     1) query to get a list of all the participants 
     2) pick a winner */
 
-    var list_of_participants = await get_list_of_participants();
-    console.log(list_of_participants)
-    const email_array = [];
-    list_of_participants = JSON.parse(JSON.stringify(list_of_participants));
-    list_of_participants.forEach(element => {
-        email_array.push(element.email)
-    });
-    console.log(email_array)
+  var list_of_participants = await get_list_of_participants();
+  console.log(list_of_participants);
+  const email_array = [];
+  list_of_participants = JSON.parse(JSON.stringify(list_of_participants));
+  list_of_participants.forEach((element) => {
+    email_array.push(element.email);
+  });
+  console.log(email_array);
 
-    const winner_email = email_array[Math.floor(Math.random() * email_array.length)];
-    req.session.winner_picked = true;
-    console.log(winner_email)
+  const winner_email =
+    email_array[Math.floor(Math.random() * email_array.length)];
+  req.session.winner_picked = true;
+  console.log(winner_email);
 
-    /* Create paypal payment */
-    var create_payment_json = {
-        "intent": "sale",
-        "payer": {
-            "payment_method": "paypal"
+  /* Create paypal payment */
+  var create_payment_json = {
+    intent: "sale",
+    payer: {
+      payment_method: "paypal",
+    },
+    redirect_urls: {
+      return_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    },
+    transactions: [
+      {
+        item_list: {
+          items: [
+            {
+              name: "Lottery",
+              sku: "Funding",
+              price: req.session.paypal_amount,
+              currency: "USD",
+              quantity: 1,
+            },
+          ],
         },
-        "redirect_urls": {
-            "return_url": "http://localhost:3000/success",
-            "cancel_url": "http://localhost:3000/cancel"
+        amount: {
+          currency: "USD",
+          total: req.session.paypal_amount,
         },
-        "transactions": [{
-            "item_list": {
-                "items": [{
-                    "name": "Lottery",
-                    "sku": "Funding",
-                    "price": req.session.paypal_amount,
-                    "currency": "USD",
-                    "quantity": 1
-                }]
-            },
-            "amount": {
-                "currency": "USD",
-                "total": req.session.paypal_amount
-            },
-            'payee': {
-                'email': winner_email
-            },
-            "description": "Paying the winner of the lottery application"
-        }]
-    };
+        payee: {
+          email: winner_email,
+        },
+        description: "Paying the winner of the lottery application",
+      },
+    ],
+  };
 
-    paypal.payment.create(create_payment_json, function (error, payment) {
-        if (error) {
-            throw error;
-        } else {
-            console.log("Create Payment Response");
-            console.log(payment);
-            for(var i=0; i < payment.links.length; i++){
-                console.log(payment.links[i].rel)
-                if(payment.links[i].rel == 'approval_url'){
-                    return res.redirect(payment.links[i].href); //redirect user 
-                }
-            }
+  paypal.payment.create(create_payment_json, function (error, payment) {
+    if (error) {
+      throw error;
+    } else {
+      console.log("Create Payment Response");
+      console.log(payment);
+      for (var i = 0; i < payment.links.length; i++) {
+        console.log(payment.links[i].rel);
+        if (payment.links[i].rel == "approval_url") {
+          return res.redirect(payment.links[i].href); //redirect user
         }
-    });
-
-
+      }
+    }
+  });
 });
 
-
-
-
-
-
 app.listen(3000, () => {
-    console.log("server is running on port 3000!")
+  console.log("server is running on port 3000!");
 });
